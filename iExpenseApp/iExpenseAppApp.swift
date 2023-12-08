87d4248b33cbb6c45477964840b34058c8bc9d97
jkwon-dev <kwonejk@gmail.com>
2023-12-08 16:17:36 +0900

    Initial Commit

---
+++ iExpenseApp/iExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseAppApp.swift
+//  iExpenseApp
+//
+//  Created by kwon eunji on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
